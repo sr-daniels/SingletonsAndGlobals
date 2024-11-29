dd97a21bb0b33672c2fa21addd11f76f3a842ece
Sharonda Daniels <sharonda.d27@gmail.com>
2024-11-28 22:16:36 -0800

    Made shared instance mutable and added mark for Singleton design pattern

--- SingletonsAndGlobals/Singleton.swift
+++ SingletonsAndGlobals/Singleton.swift
@@ -7,12 +7,14 @@
 
 import UIKit
 
+// MARK: - Singleton
+
 /// This is an example of a Singleton pattern: you are not able to make your own ApiClient via ApiClient(). A Singleton is a class that has only one instance whereas it provides a single point of access to that instance. In this example, the "ApiClient" class is the Singleton class which contains the "shared" property contains the class' only instance and the only way to access this instance is via "ApiClient.shared".
 struct LoggedInUser {}
 
 class ApiClient {
     // On the first call the shared is initalized by creating a new instance of ApiClient(), on subsequent calls it returns the same instance that was made during the first call
-    static var shared = ApiClient()
+    static let shared = ApiClient()
     
     // A private initializer is declared, confirming the prohibition of an initializer declaration that is visible to the module
     private init() {}
