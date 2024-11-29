2b91bce14f58158dc5769b37c7c5c77773f76ab6
Sharonda Daniels <sharonda.d27@gmail.com>
2024-11-28 20:31:28 -0800

    Initial Commit

---
+++ SingletonsAndGlobalsTests/SingletonsAndGlobalsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SingletonsAndGlobalsTests.swift
+//  SingletonsAndGlobalsTests
+//
+//  Created by Sharonda Daniels on 11/28/24.
+//
+
+import Testing
+@testable import SingletonsAndGlobals
+
+struct SingletonsAndGlobalsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
